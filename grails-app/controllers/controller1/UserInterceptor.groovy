5a9af79c61518881505de8505e49b7426fe341bc
sahi123 <prashant.git91@gmail.com>
2020-03-19 13:19:40 +0530

    controller specific interceptor.

---
+++ grails-app/controllers/controller1/UserInterceptor.groovy
@@ -0,0 +1,29 @@
+/*
+package controller1
+
+
+class UserInterceptor {
+
+    boolean before() {
+        println params
+        println "inside Before User Interceptor>>>>>>>"
+        println "controllerName : ${controllerName}\t actionName: ${actionName}"
+        true
+    }
+
+    boolean after() {
+        println "inside After User Interceptor>>>>>>>"
+        println "controllerName : ${controllerName}\t actionName: ${actionName}"
+        println "View : ${view}"
+        println "model : ${model}"
+        model.userList = [model.userList.first()]
+        true
+    }
+
+    void afterView() {
+        println "inside After View User Interceptor>>>>>>>"
+        println "controllerName : ${controllerName}\t actionName: ${actionName}"
+        // no-op
+    }
+}
+*/
